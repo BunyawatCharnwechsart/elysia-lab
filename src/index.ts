aed10ae938fa79f529ec5e182bb2fe36518d8abe
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-05-04 18:02:12 +0700

    add eden

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { users } from "./users";
 import { menus } from "./menus";
 import { auth } from "../modules/auth";
 
-new Elysia()
+const app = new Elysia()
   .get('/', () => 'hello elysia')
   .use(auth)
   .use(users)
   .use(menus)
   .listen(3000);
+
+export type App = typeof app;
