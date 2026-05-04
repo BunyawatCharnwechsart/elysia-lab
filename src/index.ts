89816cce4a0577e6fa03eba2693247a5a706881b
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-05-04 18:45:42 +0700

    remove eden

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import { menus } from "./menus";
 import { auth } from "../modules/auth";
 import { cors } from "@elysiajs/cors";
 
-const app = new Elysia()
+new Elysia()
   .get('/', () => 'hello elysia')
   .use(cors())
   .use(auth)
   .use(users)
   .use(menus)
   .listen(3000);
-
-export type App = typeof app;
