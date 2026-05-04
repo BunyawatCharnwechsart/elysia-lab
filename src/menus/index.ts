c6f1a93dc02e468bdb28ffbf198b75719f3992bd
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-05-04 14:11:30 +0700

    add post menus name, price

--- src/menus/index.ts
+++ src/menus/index.ts
@@ -2,4 +2,29 @@ import Elysia, { t } from "elysia";
 import { supabase } from "../../libs/supabase";
 
 export const menus = new Elysia()
-    .get('/menus', async ()=> 'this is menus')
+    .get('/menus', async () => {
+        const { data, error } = await supabase.from("menus").select("*")
+        if (error) throw error
+        return data
+    })
+    .post('/menus', async ({body}) => {
+        const { name, price } = body;
+        const { data, error } = await supabase
+            .from("menus")
+            .insert({ name, price })
+            .select()
+            .single()
+        if (error) throw error
+
+        return {
+            message: "add menus secuss",
+            menu : data
+        };
+    },
+    {
+        body: t.Object({
+            name: t.String({ minLength: 1 }),
+            price: t.Number({ minimum: 0 }),
+        })
+    }
+)
